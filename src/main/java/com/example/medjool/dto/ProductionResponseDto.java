902b3af25f1934f09e6764fb5ecb337aef0b4c17
salaheddinesamid <143968052+salaheddinesamid@users.noreply.github.com>
2025-06-09 16:09:16 +0100

    update

--- src/main/java/com/example/medjool/dto/ProductionResponseDto.java
+++ src/main/java/com/example/medjool/dto/ProductionResponseDto.java
@@ -1,4 +1,16 @@
 package com.example.medjool.dto;
 
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
 public class ProductionResponseDto {
+
+    private Long orderId;
+    private LocalDateTime productionStartDate;
+    private double workingHours;
+    private double remainingHours;
+    private String status;
+
 }
