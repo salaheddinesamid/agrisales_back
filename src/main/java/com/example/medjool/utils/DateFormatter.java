4c67765f70b8f6c84d5af73387987034a5b5ce03
salaheddinesamid <143968052+salaheddinesamid@users.noreply.github.com>
2025-06-21 11:26:06 +0100

    add a class util for date formatting MM-DD-YYYY

--- src/main/java/com/example/medjool/utils/DateFormatter.java
+++ src/main/java/com/example/medjool/utils/DateFormatter.java
@@ -1,4 +1,16 @@
 package com.example.medjool.utils;
 
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Component
 public class DateFormatter {
+
+    public String formatDate(LocalDateTime date) {
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        return date.format(dateFormatter);
+    }
+
 }
