509aa2f59da636c398b4c40feeee91ddc4a6d296
salaheddinesamid <143968052+salaheddinesamid@users.noreply.github.com>
2025-06-23 18:19:11 +0100

    update

--- src/main/java/com/example/medjool/model/MixedOrderItem.java
+++ src/main/java/com/example/medjool/model/MixedOrderItem.java
@@ -1,4 +1,25 @@
 package com.example.medjool.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "mixed_order_items")
 public class MixedOrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @OneToMany(mappedBy = "mixedOrderItem", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<MixedOrderItemDetails> itemDetails;
 }
--- src/main/java/com/example/medjool/model/MixedOrderItemDetails.java
+++ src/main/java/com/example/medjool/model/MixedOrderItemDetails.java
@@ -1,4 +1,31 @@
 package com.example.medjool.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+
+@Table(name = "mixed_order_item_details")
 public class MixedOrderItemDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "mixed_order_id", nullable = false)
+    private  MixedOrderItem mixedOrderItem;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(name = "percentage",nullable = true)
+    private double percentage;
+
+    @Column(name = "weight", nullable = true)
+    private double weight;
 }
