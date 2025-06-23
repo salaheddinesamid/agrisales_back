23b75d8c3b46349a6fa3746e4916d7c2f2a223db
salaheddinesamid <143968052+salaheddinesamid@users.noreply.github.com>
2025-06-23 18:10:58 +0100

    update mixed order details

--- src/main/java/com/example/medjool/dto/MixedOrderDto.java
+++ src/main/java/com/example/medjool/dto/MixedOrderDto.java
@@ -1,4 +1,15 @@
 package com.example.medjool.dto;
 
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class MixedOrderDto {
+
+    private List<MixedOrderItemRequestDto> items;
+    private String brand;
+    private int palletId;
+    private double totalWeight;
+    private double totalPrice;
 }
--- src/main/java/com/example/medjool/dto/MixedOrderItemRequestDto.java
+++ src/main/java/com/example/medjool/dto/MixedOrderItemRequestDto.java
@@ -1,4 +1,14 @@
 package com.example.medjool.dto;
 
+import lombok.Data;
+
+
+@Data
 public class MixedOrderItemRequestDto {
+
+    private long productId;
+    private String productCode;
+    private double percentage;
+    private double pricePerKg;
+    private double weight;
 }
