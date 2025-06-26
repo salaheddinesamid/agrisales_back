30070ed9755054aca254fe0d0a472bf04ee3f2b5
salaheddinesamid <143968052+salaheddinesamid@users.noreply.github.com>
2025-06-26 11:17:16 +0100

    update

--- src/main/java/com/example/medjool/exception/ConfigurationExceptionController.java
+++ src/main/java/com/example/medjool/exception/ConfigurationExceptionController.java
@@ -1,4 +1,14 @@
 package com.example.medjool.exception;
 
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class ConfigurationExceptionController {
+
+    @ExceptionHandler(PalletNotFoundException.class)
+    public String handlePalletNotFoundException(PalletNotFoundException ex) {
+        // Log the exception or perform any other necessary actions
+        return "error/pallet-not-found"; // Return the view name for the error page
+    }
 }
