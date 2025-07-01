24d2340fb1a63a3dc437456a6124125eaba76552
salaheddinesamid <143968052+salaheddinesamid@users.noreply.github.com>
2025-07-01 11:59:01 +0100

    add a get request mapping to handle marge per client requests

--- src/main/java/com/example/medjool/controller/OverviewController.java
+++ src/main/java/com/example/medjool/controller/OverviewController.java
@@ -1,4 +1,27 @@
 package com.example.medjool.controller;
 
+import com.example.medjool.dto.MarginClientResponseDto;
+import com.example.medjool.dto.OrderCostDto;
+import com.example.medjool.services.implementation.OverviewServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/margin_per_client")
 public class OverviewController {
+
+    private final OverviewServiceImpl overviewService;
+
+    @Autowired
+    public OverviewController(OverviewServiceImpl overviewService) {
+        this.overviewService = overviewService;
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<MarginClientResponseDto> getMarginPerClient(@RequestParam String companyName) {
+        return overviewService.getMarginPerClient(companyName);
+    }
 }
