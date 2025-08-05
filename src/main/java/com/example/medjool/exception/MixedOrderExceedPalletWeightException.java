bbd766a5cb6ca13b7b8c218ec080a033e1670cbb
salaheddinesamid <143968052+salaheddinesamid@users.noreply.github.com>
2025-08-05 09:25:11 +0100

    update

---
+++ src/main/java/com/example/medjool/exception/MixedOrderExceedPalletWeightException.java
@@ -0,0 +1,7 @@
+package com.example.medjool.exception;
+
+public class MixedOrderExceedPalletWeightException extends RuntimeException {
+  public MixedOrderExceedPalletWeightException(String message) {
+    super(message);
+  }
+}
