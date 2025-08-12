f1dd4dc60e1791b131446a500dae2504b018d74c
salaheddinesamid <143968052+salaheddinesamid@users.noreply.github.com>
2025-08-12 17:15:33 +0100

    add new exception handler to handle forex not found

---
+++ src/main/java/com/example/medjool/exception/ForexNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.medjool.exception;
+
+public class ForexNotFoundException extends RuntimeException {
+    public ForexNotFoundException(String message) {
+        super(message);
+    }
+}
